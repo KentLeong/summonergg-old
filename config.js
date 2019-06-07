ab4b8eb8962e6274153427ca104e9b459e199f85
kentleong <leongzeno@gmail.com>
2019-06-07 12:14:13 -0700

    update

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/sgg_na",
-  riot: "RGAPI-0429bf08-e933-49e2-807e-377df187514d",
+  riot: "RGAPI-d5be9096-6383-4db1-8bb5-3b5fdf70a964",
   ver: "9.11.1",
   host: "http://localhost"
 }
 
 //RGAPI-4c3e5b9d-b371-4492-bc32-bfe17bf2b300
-//RGAPI-0429bf08-e933-49e2-807e-377df187514d
+//RGAPI-d5be9096-6383-4db1-8bb5-3b5fdf70a964
