872e0464a2413da51321e7307daa0d47865ec201
Eric <leongzeno@gmail.com>
2019-06-10 11:42:22 -0700

    updated key

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   mongoURI: "mongodb://localhost:27017/sgg_na",
-  riot: "RGAPI-15cd2b05-e5db-443c-8567-ebe8cb895576",
+  riot: "RGAPI-7985b1cf-cb8c-4df9-ae82-bf41748225f7",
   ver: "9.11.1",
   host: "http://na.localhost.gg"
 }
 
 //RGAPI-4c3e5b9d-b371-4492-bc32-bfe17bf2b300
-//RGAPI-15cd2b05-e5db-443c-8567-ebe8cb895576
+//RGAPI-7985b1cf-cb8c-4df9-ae82-bf41748225f7
