703f006db2afb75950d6bf06c6c3815dd3d3b690
Eric <leongzeno@gmail.com>
2019-06-10 18:46:31 -0700

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   mongoURI: "mongodb://localhost:27017/sgg_na",
   riot: "RGAPI-7985b1cf-cb8c-4df9-ae82-bf41748225f7",
   ver: "9.11.1",
-  host: "http://na.localhost.gg"
+  host: "http://na.summoner.gg"
 }
 
 //RGAPI-4c3e5b9d-b371-4492-bc32-bfe17bf2b300
