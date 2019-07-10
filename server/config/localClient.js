20cc6cd10bb1f508c9fd4449d95e630cd19b93f8
Eric <leongzeno@gmail.com>
2019-07-10 12:42:52 -0700

    update

--- server/config/localClient.js
+++ server/config/localClient.js
@@ -2,6 +2,7 @@ var axios = require('axios');
 
 module.exports = (region) => {
   return axios.create({
-    baseURL: `http://${region}.localhost.gg/api`
+    baseURL: `http://${region}.summoner.gg/api`
+    // baseURL: `http://${region}.localhost.gg/api`
   })
 }
